2f842d32475b239baabd19e0109656c9d97cb48f
Varadmurty-mohod <69577585+Varadmurty-mohod@users.noreply.github.com>
2022-12-11 15:37:07 -0500

    Uni Login design update

--- AedFinalProject/src/university/uniLogin.java
+++ AedFinalProject/src/university/uniLogin.java
@@ -47,7 +47,7 @@ public class uniLogin extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(204, 0, 0));
-        jPanel1.setLayout(null);
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButtonBackUniLogin.setBackground(new java.awt.Color(255, 255, 255));
         jButtonBackUniLogin.setFont(new java.awt.Font("Times New Roman", 1, 13)); // NOI18N
@@ -58,39 +58,31 @@ public class uniLogin extends javax.swing.JFrame {
                 jButtonBackUniLoginActionPerformed(evt);
             }
         });
-        jPanel1.add(jButtonBackUniLogin);
-        jButtonBackUniLogin.setBounds(25, 21, 72, 23);
+        jPanel1.add(jButtonBackUniLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 21, -1, -1));
 
         jLabelLogoUNILogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Northeastern_University_Logo-White.png"))); // NOI18N
-        jPanel1.add(jLabelLogoUNILogin);
-        jLabelLogoUNILogin.setBounds(169, 44, 549, 181);
+        jPanel1.add(jLabelLogoUNILogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 90, 350, 140));
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Username:");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(326, 301, 67, 17);
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(326, 301, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Password:");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(326, 342, 61, 17);
-        jPanel1.add(btnUsername);
-        btnUsername.setBounds(409, 298, 131, 22);
-        jPanel1.add(btnPassword);
-        btnPassword.setBounds(406, 339, 134, 22);
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(326, 342, -1, -1));
+        jPanel1.add(btnUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(409, 298, 131, -1));
+        jPanel1.add(btnPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(406, 339, 134, -1));
 
         jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Role:");
-        jPanel1.add(jLabel3);
-        jLabel3.setBounds(326, 383, 33, 17);
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(326, 383, -1, -1));
 
         selectUniRole.setForeground(new java.awt.Color(255, 255, 255));
         selectUniRole.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Student", "Professor", "University Admin" }));
-        jPanel1.add(selectUniRole);
-        selectUniRole.setBounds(406, 380, 125, 22);
+        jPanel1.add(selectUniRole, new org.netbeans.lib.awtextra.AbsoluteConstraints(406, 380, -1, -1));
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setFont(new java.awt.Font("Times New Roman", 1, 13)); // NOI18N
@@ -101,28 +93,25 @@ public class uniLogin extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(401, 421, 72, 23);
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(401, 421, -1, -1));
 
         jLabelTitlelogin.setFont(new java.awt.Font("Times New Roman", 3, 24)); // NOI18N
         jLabelTitlelogin.setForeground(new java.awt.Color(255, 255, 255));
         jLabelTitlelogin.setText("University Login");
-        jPanel1.add(jLabelTitlelogin);
-        jLabelTitlelogin.setBounds(354, 243, 167, 29);
+        jPanel1.add(jLabelTitlelogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(354, 243, -1, -1));
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/toppng.com-vignette-png-19201080-overlay-photo-1920x1080.png"))); // NOI18N
-        jPanel1.add(jLabel4);
-        jLabel4.setBounds(0, 0, 900, 510);
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 510));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 894, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 507, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
